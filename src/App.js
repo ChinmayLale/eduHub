188061f37b6d29e6a353a344f334aa705df005ed
ChinmayLale <chinmay29.lale@gmail.com>
2024-03-04 23:31:14 +0530

    NavBar 80% Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
+import NavBar from './Components/NavBar';
 function App() {
+  const [mode , setMode]  =useState('light');
+  const changeMode = () => {
+    if(mode==='light'){
+      setMode('dark');
+      // 222831
+      document.body.style.backgroundColor = '#252A34';
+      document.body.style.Color = 'white';
+    }
+    else{
+      setMode('light');
+      // FEFBF6
+      document.body.style.backgroundColor = '#EEEEEE';
+      document.body.style.Color = 'black';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar mod={mode} changemode = {changeMode}/>
+    </>
   );
 }
 
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import edulogo1 from '../assets/edulogo_1.svg';
+// import edulogo2 from '../assets/edulogo_2.svg';
+// import edulogo3 from '../assets/edulogo_3.svg';
+function NavBar(props) {
+    return (
+        <div className="navbar">
+            <div className="logo">
+                <img src={edulogo1} alt="" style={{ width: '50px', height: '50px'}}/>
+                <h2>Edu<span style={{ color: props.mod==='light'? '#561C24' :'crimson' }}>Hub</span></h2>
+            </div>
+            <div className="menu">
+                <h3><a href="#/">Home</a></h3>
+                <h3><a href="#/">About</a></h3>
+                <h3><a href="#/">Courses</a></h3>
+                <h3><a href="#/">Reviews</a></h3>
+                <h3><a href="#/">Instructors</a></h3>
+            </div>
+            <div className="login">
+                <button className='login-btn'>Login / Signup</button>
+                <label htmlFor="theme" className="theme" >
+                    <span className="theme__toggle-wrap">
+                        <input id="theme" className="theme__toggle" type="checkbox" role="switch" name="theme" value="dark" onChange={props.changemode} />
+                        <span className="theme__fill"></span>
+                        <span className="theme__icon">
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                            <span className="theme__icon-part"></span>
+                        </span>
+                    </span>
+                </label>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
