6da2a206e87ab63904142f5c59172f983aa98678
ChinmayLale <chinmay29.lale@gmail.com>
2024-03-10 21:51:16 +0530

    Before adding DB all working good ..

--- src/Components/DataBaseConnect.jsx
+++ src/Components/DataBaseConnect.jsx
@@ -1,7 +1,1 @@
 import mongoose , {connect} from 'mongoose';
-
-const dbUrl = 'mongodb+srv://astrochinmay:astrochinmay@eduhub.2sgtwed.mongodb.net/?retryWrites=true&w=majority&appName=EduHub';
-
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected TO Mongo "))
-  .catch((err) => console.error(err));
