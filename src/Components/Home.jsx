181898a6aafdfe085030a9db31dedc77a2b80d77
ChinmayLale <chinmay29.lale@gmail.com>
2024-03-11 22:09:16 +0530

    Fixed Overlapping issue of home components , all good ..

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,33 +21,34 @@ function Home() {
 
   return (
     <div className="hero">
-      <div className="left">
-        <h1>
-          Go beyond the limitations of <span style={{ color: '#1a2e35' }}>e-learning</span>
-          {/* #0d594f */}
-        </h1>
-        <h2>Empower colabrative learning  Anytime Anywhere Togather !</h2>
-        <div className="btns">
-          <button className="animated-button">
-            <svg xmlns="http://www.w3.org/2000/svg" className="arr-2" viewBox="0 0 24 24">
-              <path
-                d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-              ></path>
-            </svg>
-            <span className="text">Get Started !</span>
-            <span className="circle"></span>
-            <svg xmlns="http://www.w3.org/2000/svg" className="arr-1" viewBox="0 0 24 24">
-              <path
-                d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-              ></path>
-            </svg>
-          </button>
+      <div className="hero-content">
+        <div className="left">
+          <h1>
+            Go beyond the limitations of <span style={{ color: '#1a2e35' }}>e-learning</span>
+            {/* #0d594f */}
+          </h1>
+          <h2>Empower colabrative learning  Anytime Anywhere Togather !</h2>
+          <div className="btns">
+            <button className="animated-button">
+              <svg xmlns="http://www.w3.org/2000/svg" className="arr-2" viewBox="0 0 24 24">
+                <path
+                  d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+                ></path>
+              </svg>
+              <span className="text">Get Started !</span>
+              <span className="circle"></span>
+              <svg xmlns="http://www.w3.org/2000/svg" className="arr-1" viewBox="0 0 24 24">
+                <path
+                  d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className="right">
+          <img src={anim1} alt="" className='heroimg' />
         </div>
       </div>
-      <div className="right">
-        <img src={anim1} alt="" className='heroimg' />
-      </div>
-
 
 
 
@@ -79,11 +80,11 @@ function Home() {
                     Front-end development&#160;+ jQuery&#160;+ CMS
                   </div>
                   <div className="ag-courses-item_date-box">
-                      Start:
-                      <span className="ag-courses-item_date">
-                        04.11.2022
-                      </span>
-                    </div>
+                    Start:
+                    <span className="ag-courses-item_date">
+                      04.11.2022
+                    </span>
+                  </div>
                 </a>
               </div>
             </div>
